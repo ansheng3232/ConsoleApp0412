e148b67073e78d2e16edd9ac0e0bba7d77cc0422
Chen An <v-chenan1@microsoft.com>
2022-04-21 07:18:52 +0000

    all

--- WebApplication12/WeatherForecast.cs
+++ WebApplication12/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace WebApplication12
             set; }
 
         public int TemperatureC { get; set; }
-        //123
+        //123123
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
